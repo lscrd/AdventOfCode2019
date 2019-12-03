49215f998c5b54d8d1298f535756f4067b986b4c
Luc Secouard <luc.secouard@laposte.net>
2019-12-03 16:28:43 +0100

    Add files via upload

---
+++ Day 01/p1.nim
@@ -0,0 +1,30 @@
+import strutils
+
+var moduleMasses: seq[int]
+for line in "data".lines:
+  moduleMasses.add(line.parseInt())
+
+########################################################################
+# Part 1
+
+var fuelMass1 = 0
+for moduleMass in moduleMasses:
+  fuelMass1 += moduleMass div 3 - 2
+
+echo "Part 1: ", fuelMass1
+
+########################################################################
+# Part 2
+
+## Compute the fuel mass needed for a module, including the fuel itself.
+proc fuelMass(moduleMass: int): int =
+  var mass = moduleMass div 3 - 2
+  while mass > 0:
+    result += mass
+    mass = mass div 3 - 2
+
+var fuelMass2 = 0
+for moduleMass in moduleMasses:
+  fuelMass2 += fuelMass(moduleMass)
+
+echo "Part 2: ", fuelMass2
