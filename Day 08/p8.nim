fbf6e8c992ab65bdd405bfa9cab3d00609e8ece0
Luc Secouard <luc.secouard@laposte.net>
2019-12-09 11:11:39 +0100

    Add files via upload

---
+++ Day 08/p8.nim
@@ -0,0 +1,52 @@
+import strutils
+
+const
+  IMAGE_WIDTH = 25
+  IMAGE_HEIGHT = 6
+  LAYER_SIZE = IMAGE_WIDTH * IMAGE_HEIGHT
+
+type Color = range['0'..'2']
+
+let data = readFile("data").strip()
+
+# Extract layers from data.
+var layers: seq[string]
+for i in countup(0, data.high, LAYER_SIZE):
+  layers.add(data[i..<(i + LAYER_SIZE)])
+
+
+###############################################################################
+# Part 1
+
+var counts: array[Color, int]     # Counts of '0', '1' and '2'.
+var minCount = 1_000_000_000      # Minimal count of '0'.
+var result: int                   # Result for part one.
+
+for i in 0..layers.high:
+  counts = [0, 0, 0]
+  for c in layers[i]:
+    inc counts[c]
+  if counts['0'] < minCount:
+    minCount = counts['0']
+    result = counts['1'] * counts['2']
+
+echo "\nPart 1: ", result
+
+
+###############################################################################
+# Part 2
+
+var image = newString(layers[0].len)
+
+# Build the image.
+for i in 0..image.high:
+  var color: Color
+  for layer in layers:
+    color = layer[i]
+    if color != '2': break  # Found white or black.
+  image[i] = if color == '1': '#' else: ' '  # Using '#' for better legibility.
+
+# Display the image.
+echo "\nPart 2:\n"
+for i in countup(0, image.high, IMAGE_WIDTH):
+  echo image[i..<(i + IMAGE_WIDTH)]
