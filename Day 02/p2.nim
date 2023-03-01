80b6e3555b2d1dc8a4ddde2ceedb0dd87d7e0a03
Luc Secouard <luc.secouard@laposte.net>
2023-03-01 12:56:51 +0100

    Add files via upload
    Minor changes.

--- Day 02/p2.nim
+++ Day 02/p2.nim
@@ -1,5 +1,4 @@
-import sequtils
-import strutils
+import std/[sequtils, strutils]
 
 import ../common/intcode
 
@@ -11,11 +10,9 @@ let data = readFile("data").strip().split(',')
 let program = map(data, parseInt)
 
 
-#####################################################################
-# Part 1
+### Part 1 ###
 
 var computer: Computer
-
 computer.init(program)
 computer.update(12, 2)
 computer.run()
@@ -23,20 +20,19 @@ computer.run()
 echo "Part 1: ", computer.memory[0]
 
 
-#####################################################################
-# Part 2
+### Part 2 ###
 
-const VALRANGE = 0..99
-const TARGET = 19690720
+const ValRange = 0..99
+const Target = 19690720
 
 var result: int
 block search:
-  for verb in VALRANGE:
-    for noun in VALRANGE:
+  for verb in ValRange:
+    for noun in ValRange:
       computer.init(program)
       computer.update(noun, verb)
       computer.run()
-      if computer.memory[0] == TARGET:
+      if computer.memory[0] == Target:
         result = 100 * noun + verb
         break search
 
