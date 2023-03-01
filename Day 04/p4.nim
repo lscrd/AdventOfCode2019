28039a37f664ee035079cf5c7582a8ff99d54c9b
Luc Secouard <luc.secouard@laposte.net>
2023-03-01 13:32:02 +0100

    Add files via upload
    Added a data file from which read the puzzle input.
    Other minor changes.

--- Day 04/p4.nim
+++ Day 04/p4.nim
@@ -1,39 +1,41 @@
-## Not the most elegant, but very fast.
+# Not the most elegant, but very fast.
 
-const
-  MIN = 134792
-  MAX = 675810
+import std/[strscans, strutils]
 
-type Digit = range[0..9]
+type Digit = 0..9
 
-## Compute a number from a list of digits.
 func number(digits: varargs[Digit]): int =
+  ## Compute a number from a list of digits.
   for d in digits:
     result = 10 * result + d
 
+# Read minimum and maximum allowed passwords.
+var minval, maxval: int
+let input = readFile("p4.data").strip()
+discard input.scanf("$i-$i", minval, maxval)
 
-############################################################################
-# Part 1
+
+### Part 1 ###
 
 var count1 = 0
 
 block loop1:
-  for d1 in 1.Digit..9:
+  for d1 in Digit(1)..Digit(9):
     for d2 in d1..9:
       for d3 in d2..9:
         for d4 in d3..9:
           for d5 in d4..9:
             for d6 in d5..9:
               let n = number(d1, d2, d3, d4, d5, d6)
-              if n < MIN: continue
-              if n > MAX: break loop1
+              if n < minval: continue
+              if n > maxval: break loop1
               if d1 == d2 or d2 == d3 or d3 == d4 or d4 == d5 or d5 == d6:
                 inc count1
 
 echo "Part 1: ", count1
 
-############################################################################
-# Part 2
+
+### Part 2 ###
 
 var count2 = 0
 
@@ -45,8 +47,8 @@ block loop2:
           for d5 in d4..9:
             for d6 in d5..9:
               let n = number(d1, d2, d3, d4, d5, d6)
-              if n < MIN: continue
-              if n > MAX: break loop2
+              if n < minval: continue
+              if n > maxval: break loop2
               if (d2 == d1 and d2 != d3) or
                  (d3 == d2 and d3 notin {d1, d4}) or
                  (d4 == d3 and d4 notin {d2, d5}) or
